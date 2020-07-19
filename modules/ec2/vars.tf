c0c694311ec862edbb0676a1d42f7cb03804fecd
unknown <anthble@hotmail.com>
2020-07-19 22:30:36 +0100

    reinitialized files

---
+++ modules/ec2/vars.tf
@@ -0,0 +1,20 @@
+variable "ec2_count" {
+    default = "1"
+}
+
+variable "ami_id" {
+    default     = "ami-0cf94b1c148cb4b81"
+}
+
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "subnet_id" {
+ 
+}
+
+variable "region" {
+    default = "eu-west-2"
+}
